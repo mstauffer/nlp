cd63a371276b7c8002099952726f9035881d7671
mstauffer <matheusvostauffer@gmail.com>
2024-11-24 04:21:25 -0300

    update: adiciona tipagem às funções e parâmetro para suprimir os outputs na função get_cls_results

--- atividade_3/utils.py
+++ atividade_3/utils.py
@@ -2,11 +2,24 @@ from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.metrics import classification_report
 from sklearn.metrics import precision_recall_fscore_support
 
+from sklearn.feature_extraction.text import _VectorizerMixin
+from sklearn.base import BaseEstimator
+from sklearn.pipeline import Pipeline
+
+from typing import List, Optional
 import matplotlib.pyplot as plt
 import pandas as pd
 import time
 
-def custom_cls(results, model_label, split, model=None, vectorizer=None, pipe=None):
+def custom_cls(
+    results: pd.DataFrame,
+    model_label: str,
+    split: List[pd.Series],
+    model: Optional[BaseEstimator]=None,
+    vectorizer: Optional[_VectorizerMixin]=None,
+    pipe: Optional[Pipeline]=None
+):
+
     X_train, X_test, y_train, y_test = split[0], split[1], split[2], split[3]
                                                         
     start_time = time.time()
@@ -32,19 +45,28 @@ def custom_cls(results, model_label, split, model=None, vectorizer=None, pipe=No
 
     return df_concat, predictions
 
-def show_cls_results(results, model_label, split, model=None, vectorizer=None, pipe=None):
+def get_cls_results(
+    results: pd.DataFrame,
+    model_label: str,
+    split: List[pd.Series],
+    show_results: bool=True,
+    model: Optional[BaseEstimator]=None,
+    vectorizer: Optional[_VectorizerMixin]=None,
+    pipe: Optional[Pipeline]=None
+):
     
     df_concat, preds = custom_cls(results, model_label, split, model, vectorizer, pipe)
     y_test = split[3]
     
-    print(classification_report(y_test, preds))
+    if show_results:
+        print(classification_report(y_test, preds))
 
-    ConfusionMatrixDisplay.from_predictions(
-        y_true=y_test,
-        y_pred=preds,
-        xticks_rotation=90
-    )
+        ConfusionMatrixDisplay.from_predictions(
+            y_true=y_test,
+            y_pred=preds,
+            xticks_rotation=90
+        )
 
-    plt.show()
+        plt.show()
     
     return df_concat.sort_values(by=['F1-Score'], ascending=False)
